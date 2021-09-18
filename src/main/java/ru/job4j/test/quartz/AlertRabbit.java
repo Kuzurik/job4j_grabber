278b4bb5aa71b2c341d3891c33a6cfe440c271fe
Alex <vo>
2021-09-18 16:28:15 +0300

    1. Quartz [#175122 #111827]

---
+++ src/main/java/ru/job4j/test/quartz/AlertRabbit.java
@@ -0,0 +1,53 @@
+package ru.job4j.test.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class AlertRabbit {
+
+    private final Properties properties = new Properties();
+
+    public void init() {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            properties.load(in);
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+    }
+
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    public static void main(String[] args) {
+        AlertRabbit rabbit = new AlertRabbit();
+        rabbit.init();
+        int interval = Integer.parseInt(rabbit.properties.getProperty("rabbit.interval"));
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+}
